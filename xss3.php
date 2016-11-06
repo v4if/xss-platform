6ee10f7f6eb8fadaa51087f8f6c1c95a2a20af22
v4if <atkjest@gmail.com>
2016-11-06 16:39:34 -0500

    xss-platform

--- xss3.php
+++ xss3.php
@@ -3,11 +3,11 @@
  * @Author: v4if
  * @Date:   2016-11-06 11:57:15
  * @Last Modified by:   v4if
- * @Last Modified time: 2016-11-06 16:11:48
+ * @Last Modified time: 2016-11-06 16:39:19
  */
 // 开启报错信息
-ini_set("display_errors", "On");
-error_reporting(E_ALL | E_STRICT);
+// ini_set("display_errors", "On");
+// error_reporting(E_ALL | E_STRICT);
 
 /**
  * 获取环境变量
@@ -32,9 +32,22 @@ $password = env("MYSQL_PASSWORD", "secret");
 
 
 # 连接到数据库
-$conn = mysql_connect($serverName.':'.$port, $username, $password) or die('Cannot connect to MySQL');
-mysql_select_db($databaseName, $conn) or die('Cannot connect to the database');
-mysql_query('SET NAMES utf8');
+$mysqli = new mysqli($serverName.":".$port, $username, $password, $databaseName);  
+if(!$mysqli)  {  
+    echo"database error";  
+}else{  
+    echo"php env successful";  
+}  
+$mysqli->close();  
 
-echo "done";
+// $mysqli = new mysqli("localhost", "root", "", "student");
+// $sql = "SELECT * FROM tb_user";
+// $result = $mysqli->query($sql);
+// $row = $result->fetch_assoc(); // 从结果集中取得一行作为关联数组
+// echo $row["password"];
+// /* free result set */
+// $result->free();
+
+// /* close connection */
+// $mysqli->close();
 ?>
