54f970051fd010d11d9f12c4b7d783f017577992
v4if <atkjest@gmail.com>
2016-11-06 16:08:28 -0500

    xss-platform

--- xss3.php
+++ xss3.php
@@ -2,15 +2,15 @@
 /**
  * @Author: v4if
  * @Date:   2016-11-06 11:57:15
- * @Last Modified by:   root
- * @Last Modified time: 2016-11-06 16:02:52
+ * @Last Modified by:   v4if
+ * @Last Modified time: 2016-11-06 16:08:23
  */
 // 开启报错信息
 ini_set("display_errors", "On");
 error_reporting(E_ALL | E_STRICT);
 
 $serverName = env("MYSQL_PORT_3306_TCP_ADDR", "localhost");
-$port = env("MYSQL_PORT_3306_TCP_PORT", "3306")
+$port = env("MYSQL_PORT_3306_TCP_PORT", "3306");
 $databaseName = env("MYSQL_INSTANCE_NAME", "homestead");
 $username = env("MYSQL_USERNAME", "homestead");
 $password = env("MYSQL_PASSWORD", "secret");
