83a9d9b6548b3c1cb86a89e29140d1abf1a07fb8
v4if <atkjest@gmail.com>
2016-11-07 08:53:35 -0500

    xss-platform

--- DB.php
+++ DB.php
@@ -21,15 +21,13 @@ class DB
     function __construct()
     {
         $serverName = env("MYSQL_PORT_3306_TCP_ADDR", "localhost");
+        $serverPort = env("MYSQL_PORT_3306_TCP_PORT", "3306");
         $databaseName = env("MYSQL_INSTANCE_NAME", "homestead");
         $username = env("MYSQL_USERNAME", "homestead");
         $password = env("MYSQL_PASSWORD", "secret");
-        echo $serverName;
-        echo $databaseName;
-        echo $username;
-        echo $password;
+        echo "mysql:host=".$serverName.";dbname=".$databaseName.";port=".$serverPort;
         try {
-            $this->pdo = new PDO("mysql:host=".$serverName.";dbname=".$databaseName, $username, $password);
+            $this->pdo = new PDO("mysql:host=".$serverName.";dbname=".$databaseName.";port=".$serverPort, $username, $password);
             $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             // 检测数据库是否存在表
             $isInstall = $this->pdo->query("SHOW TABLES like 'contacts';")
